c49d544f0c87f09ebb0140fc146755a740402345
nicollaswendel <nicollaswendel1623@gmail.com>
2024-10-09 20:34:23 -0300

    feat: api test

--- CypressProject/test_spec.js
+++
@@ -1,9 +0,0 @@
-describe('Página de exemplo', () => {
-    it('Deve carregar a página inicial com sucesso', () => {
-        cy.visit('https://example.cypress.io') // Abre a página de exemplo do Cypress
-        cy.contains('type').click() // Clica em um link com o texto 'type'
-        cy.url().should('include', '/commands/actions') // Verifica se a URL mudou
-        cy.get('.action-email').type('teste@exemplo.com') // Digita em um campo de e-mail
-        cy.get('.action-email').should('have.value', 'teste@exemplo.com') // Verifica se o e-mail foi inserido corretamente
-    })
-})
